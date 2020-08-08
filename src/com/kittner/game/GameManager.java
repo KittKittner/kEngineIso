5414fdd172a772a89c12987405b5e27ca6b4ad9b
Ryan James <KittKittner@users.noreply.github.com>
2020-08-08 21:14:56 +0100

    Changes to the world generation and cell selection maths
    Changed the world to generate fully functionally (no out of bounds)
    The selected cell is now determined based on the variable geometry of the tiles/cells rather than a hard coded approximation

--- src/com/kittner/game/GameManager.java
+++ src/com/kittner/game/GameManager.java
@@ -7,7 +7,6 @@ import com.kittner.engine.gfx.Image;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.util.Arrays;
 import java.util.Random;
 
 public class GameManager extends AbstractGame
@@ -27,41 +26,77 @@ public class GameManager extends AbstractGame
         worldOriginX = newWorldOriginX;
         worldOriginY = newWorldOriginY;
 
-        tileMap = new int[worldSizeX * worldSizeY];
+        /*for(int x = 0; x < worldSizeX; x++)
+            for(int y = 0; y < worldSizeY; y++)
+                System.out.println(worldSizeX * y + x + y);*/
+
+
+        tileMap = new int[worldSizeX * worldSizeY + worldSizeX + 1];
         for(int i = 0; i < tileMap.length; i++)
+        {
+            //if(i >=1 && i < )
             tileMap[i] = rand.nextInt(5);
+        }
     }
 
     @Override
     public void update(GameContainer gc, float dt)
     {
+        //mouse coords
         mx = gc.getInput().getMouseX();
         my = gc.getInput().getMouseY();
+        //screen cell coords
         cellX = mx / tileWidth;
         cellY = my / tileHeight;
+        //screen cell offset of mouse coords
         int cellMX = mx % tileWidth;
         int cellMY = my % tileHeight;
+        //normalised offset
         float cellMXNormal = (float)cellMX / tileWidth;
         float cellMYNormal = (float)cellMY / tileHeight;
+        //the world cell the mouse is in
         selectedX = (cellY - worldOriginY) + (cellX - worldOriginX);
         selectedY = (cellY - worldOriginY) - (cellX - worldOriginX);
-        if((2.0/3.0) < Math.sqrt(Math.abs(0.5-cellMXNormal)+Math.abs(0.5-cellMYNormal))) //find the cartesian distance using normalised values
-            if(cellMXNormal < 0.5 && cellMYNormal < 0.5) //top left
-                selectedX -= 1;
-            else if(cellMXNormal >= 0.5 && cellMYNormal < 0.5) //top right
-                selectedY -= 1;
-            else if(cellMXNormal < 0.5 && cellMYNormal >= 0.5) //bottom left
-                selectedY += 1;
-            else if(cellMXNormal > 0.5 && cellMYNormal > 0.5) //bottom right
-                selectedX += 1;
+
+        //determine whether the selected tile must be changed according to the geometry of the tile and the mouse position
+        double d = Math.sqrt(Math.abs(cellMX-tileWidth/2)+Math.abs(cellMY-tileHeight/2));
+        double lb = Math.sqrt((double)tileWidth/4 + (double)tileHeight/4); //from the middle of the cell to the midpoint of the side of the tile
+        double ub = Math.sqrt((double)tileWidth/2 + (double)tileHeight/2); //from the middle of the cell to the corner of the cell
+        if(lb < d && d < ub) //if the mouse is within the four corners of the cell
+            if (cellMYNormal >= cellMXNormal) //if the mouse is in the lower left half of the cell
+            {
+                if (cellMYNormal < 0.5) //if the mouse is in the upper half of the cell
+                    selectedX -= 1;
+                else if (cellMXNormal < 0.5) //if the mouse is in left half of the cell
+                    selectedY += 1;
+                else //if the mouse is in the right half of the cell
+                    selectedX += 1;
+            }
+            else //if the mouse is in the upper right half of the cell
+            {
+                if(cellMXNormal < 0.5) //if the mouse is in the left half of the cell
+                    selectedX -= 1;
+                else if(cellMYNormal < 0.5) //if the mouse is in the upper half of the cell
+                    selectedY -= 1;
+                else //if the mouse is in the lower half of the cell
+                    selectedX += 1;
+            }
         worldX = (worldOriginX * tileWidth) + (cellX - cellY) * tileWidth / 2;
         worldY = (worldOriginY * tileHeight) + (cellX + cellY) * tileHeight / 2;
 
-        if(gc.getInput().isButtonDown(MouseEvent.BUTTON1))
-        {
-            ++tileMap[selectedY * worldSizeX + selectedX];
-            tileMap[selectedY * worldSizeX + selectedX] %= 5;
-        }
+
+        if(selectedX >= 0 && selectedX < worldSizeX && selectedY >= 0 && selectedY < worldSizeY)
+            if(gc.getInput().isButtonDown(MouseEvent.BUTTON1))
+            {
+                ++tileMap[selectedY * worldSizeX + selectedX];
+                tileMap[selectedY * worldSizeX + selectedX] %= 5;
+            }
+            else if(gc.getInput().isButtonDown(MouseEvent.BUTTON3))
+            {
+                if(tileMap[selectedY * worldSizeX + selectedX] - 1 < 0) tileMap[selectedY * worldSizeX + selectedX] = 4;
+                else --tileMap[selectedY * worldSizeX + selectedX];
+            }
+
     }
 
     @Override
@@ -70,8 +105,6 @@ public class GameManager extends AbstractGame
         for(int y = 0; y < worldSizeY; y++)
             for(int x = 0; x < worldSizeX; x++)
             {
-                //System.out.println(x + " " + y + " " + (y * worldSizeX + x));
-                //System.out.println(tileMap[y * worldSizeX + x]);
                 int[] points = calcScreenCoordsFromWorldCoords(x, y);
                 drawTile(r,
                         tileMap[y * worldSizeX + x],
@@ -126,7 +159,7 @@ public class GameManager extends AbstractGame
 
     public static void main(String[] args)
     {
-        GameContainer gc = new GameContainer(new GameManager(32, 16, 14, 10, 5 ,1),
+        GameContainer gc = new GameContainer(new GameManager(32, 16, 17, 17, 10 ,3),
                 640, 360, 2);
         gc.start();
     }
